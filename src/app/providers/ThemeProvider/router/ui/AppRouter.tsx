c47d34bfeea38d53d3b2b589d93bfd2df5baa543
gradiusalisik <gradiusalisik@gmail.com>
2023-02-12 17:40:29 +0300

    add notFound page, Loader and PageLoader

--- src/app/providers/ThemeProvider/router/ui/AppRouter.tsx
+++ src/app/providers/ThemeProvider/router/ui/AppRouter.tsx
@@ -1,21 +1,18 @@
-import { AboutPage } from "pages/AboutPage";
-import { MainPage } from "pages/MainPage";
-import React, { Suspense } from "react";
-import { Route, Routes } from "react-router-dom";
-import { routeConfig } from "shared/config/routeConfig/routeConfig";
+import { Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import { PageLoader } from 'widgets/PageLoader/PageLoader';
 
-export const AppRouter = () => {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Routes>
-        {Object.values(routeConfig).map(({ element, path }) => (
-          <Route
-            key={path}
-            path={path}
-            element={<div className="page-wrapper">{element}</div>}
-          />
-        ))}
-      </Routes>
-    </Suspense>
-  );
-};
+export const AppRouter = () => (
+  <Suspense fallback={<PageLoader />}>
+    <Routes>
+      {Object.values(routeConfig).map(({ element, path }) => (
+        <Route
+          key={path}
+          path={path}
+          element={<div className="page-wrapper">{element}</div>}
+        />
+      ))}
+    </Routes>
+  </Suspense>
+);
---
+++ src/widgets/PageLoader/PageLoader.tsx
@@ -0,0 +1,18 @@
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Loader } from 'shared/ui/Loader/Loader';
+
+import cls from './PageLoader.module.scss';
+
+interface PageLoaderProps {
+  className?: string;
+}
+
+export const PageLoader = ({ className }: PageLoaderProps) => {
+  const { t } = useTranslation();
+  return (
+    <div className={classNames(cls.pageLoader, {}, [className])}>
+      <Loader />
+    </div>
+  );
+};
